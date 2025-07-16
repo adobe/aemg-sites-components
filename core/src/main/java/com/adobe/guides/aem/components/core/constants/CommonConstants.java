4b8f350736db42b311ca200893ca6773ba406d8e
MugaboDenys <mugabodan2020@gmail.com>
2025-07-16 12:09:37 +0200

    fix banner component

--- core/src/main/java/com/adobe/guides/aem/components/core/constants/CommonConstants.java
+++ core/src/main/java/com/adobe/guides/aem/components/core/constants/CommonConstants.java
@@ -4,7 +4,7 @@ public class CommonConstants {
     public static final String SOCIAL_JSON_PATH = "/apps/guides-components/clientlibs/clientlib-hfm/clientlib-base-common/resources/json/social-share.json";
     public static final String WRITABLE_SUB_SERVICE_NAME = "guides-system-writer";
     public static final String READONLY_SUB_SERVICE_NAME = "guides-system-reader";
-    public static final String LANDING_PAGE = "/apps/aemguides/components/guides-pages/base/guides-landing-page";
+    public static final String LANDING_PAGE = "/apps/guides-components/components/guides-pages/base/guides-landing-page";
     public static final String GUIDE_DATA_PATH = "/content/data/guide";
     public static final String TOOLBAR_XF_PATH = "toolbarXFPath";
     public static final String HEADER_XF_PATH = "headerXFPath";
