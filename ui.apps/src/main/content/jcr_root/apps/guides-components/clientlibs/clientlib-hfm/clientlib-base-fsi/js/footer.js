cfd374ce21715b78067a8d7cb7909bb4b52da61b
Pruthvidhar <137395516+pruthvidhar94@users.noreply.github.com>
2025-07-17 16:34:04 +0530

    Minor Fixes after Testing
    Minor Fixes after Testing

--- ui.apps/src/main/content/jcr_root/apps/guides-components/clientlibs/clientlib-hfm/clientlib-base-fsi/js/footer.js
+++ ui.apps/src/main/content/jcr_root/apps/guides-components/clientlibs/clientlib-hfm/clientlib-base-fsi/js/footer.js
@@ -1,13 +1,20 @@
 function adjustMainHeight() {
+    const main = document.querySelector(".gu-custom-container");
+
+    if (main && main.classList.contains("EDITMODE")) {
+        return;
+    }
+
     const header = document.querySelector(".header");
     const footer = document.querySelector(".footer");
-    const main = document.querySelector(".gu-custom-container");
+    const style = window.getComputedStyle(main);
+    const marginTop = parseFloat(style.marginTop) || 0;
 
     const headerHeight = header ? header.offsetHeight : 0;
     const footerHeight = footer ? footer.offsetHeight : 0;
     const viewportHeight = window.innerHeight;
 
-    const mainHeight = viewportHeight - headerHeight - footerHeight;
+    const mainHeight = viewportHeight - headerHeight - footerHeight - marginTop;
     main.style.minHeight = `${mainHeight}px`;
 }
 
