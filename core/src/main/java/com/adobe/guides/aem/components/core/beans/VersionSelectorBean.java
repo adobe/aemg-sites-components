86906260c8d07ffabe5b68432b92bf1a0fac2755
Samba Siva Rao Kolusu <saasira@gmail.com>
2025-07-25 12:48:18 +0530

    made changes to show version-number in the version-selector dropdown and version-title as tooltip

--- core/src/main/java/com/adobe/guides/aem/components/core/beans/VersionSelectorBean.java
+++ core/src/main/java/com/adobe/guides/aem/components/core/beans/VersionSelectorBean.java
@@ -1,14 +1,20 @@
 package com.adobe.guides.aem.components.core.beans;
 
 public class VersionSelectorBean {
+    private String name;
     private String title;
     private String pagePath;
 
-    public VersionSelectorBean(String title, String pagePath) {
+    public VersionSelectorBean(String name, String title, String pagePath) {
+        this.name = name;
         this.title = title;
         this.pagePath = pagePath;
     }
 
+    public String getName() {
+        return this.name;
+    }
+    
     public String getTitle() {
         return title;
     }
