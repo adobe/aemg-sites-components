c73e5590f505b5460ebd0215f60543765a562bd5
Mithun <mithun.kd@hashouttech.com>
2025-08-26 13:51:16 +0530

    fix: manufacturing 46 41 7

--- ui.apps/src/main/content/jcr_root/apps/guides-components/clientlibs/clientlib-hfm/clientlib-base-common/js/mobile.js
+++ ui.apps/src/main/content/jcr_root/apps/guides-components/clientlibs/clientlib-hfm/clientlib-base-common/js/mobile.js
@@ -19,9 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
             event.stopPropagation(); // Prevent click from bubbling to document
             outerContainer.classList.toggle("show");
             backdrop.classList.toggle("active");
-            $('.gu-pdf-export__wrapper').removeClass('active');
-        	$('.social-links').addClass('hidden');
-        	$('.cmp-languagenavigation').css('display', 'none');
+            $('.hi-tech .gu-pdf-export__wrapper').removeClass('active');
+        	$('.hi-tech .social-links').addClass('hidden');
+        	$('.hi-tech .cmp-languagenavigation').css('display', 'none');
         });
 
         // Close menu when clicking on the backdrop
