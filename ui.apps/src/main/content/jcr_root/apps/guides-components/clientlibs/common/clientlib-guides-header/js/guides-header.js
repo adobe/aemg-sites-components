43e00e5da9f380f07be1ce26e7ff4366bf704876
Pradyuman Aggarwal <pradyumana@Pradyumans-MacBook-Pro-3.local>
2026-03-13 14:27:13 +0530

    Fixing searchbar styles

--- ui.apps/src/main/content/jcr_root/apps/guides-components/clientlibs/common/clientlib-guides-header/js/guides-header.js
+++ ui.apps/src/main/content/jcr_root/apps/guides-components/clientlibs/common/clientlib-guides-header/js/guides-header.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let hamburger = document.querySelector(".hamburger_icon");
     let header = document.querySelector(".fsi-header-wrapper");
-    let searchInput = document.getElementById("js-search-results");
+    // let searchInput = document.getElementById("js-search-results");
 
     hamburger.addEventListener('click', () => {
         header.classList.toggle('mob-navigation');
@@ -10,12 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelector('.global-search').addEventListener('click', () => {
         document.querySelector('.search-container').classList.remove('display-none');
-        searchInput.value = "";
+        // searchInput.value = "";
     });
 
     document.querySelector('.cmp-search-bar__clear-icon').addEventListener('click', () => {
         document.querySelector('.search-container').classList.add('display-none');
-        searchInput.value = "";
+        // searchInput.value = "";
     });
 
     document.querySelector('.cmp-search-bar__form').addEventListener('submit', (e) => {
